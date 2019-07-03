a121019a037591d677e722aa7ce11a6c837f4567
rajyan <38206553+rajyan@users.noreply.github.com>
2019-07-04 00:10:06 +0900

    95-D

--- ABC95/D_ver2.cpp
+++ ABC95/D_ver2.cpp
@@ -14,7 +14,7 @@
 //#define DMP(x) ((void)0)
 //#endif
 //
-//const int MOD = 1000000007, INF = 1111111111; 
+//const int MOD = 1000000007, INF = 1111111111;
 //using namespace std;
 //using lint = long long;
 //
@@ -22,35 +22,43 @@
 //
 //	cin.tie(nullptr);
 //	ios::sync_with_stdio(false);
-//	
+//
 //	int N;
 //	lint C;
 //	cin >> N >> C;
 //
-//	vector<lint> x(N), v(N);
-//	for (int i = 0; i < N; i++) cin >> x[i] >> v[i];
+//	vector<lint> x(N + 1), v(N + 1);
+//	for (int i = 1; i <= N; i++) cin >> x[i] >> v[i];
 //
-//	vector<lint> cw(N), ccw(N);
+//	vector<lint> rcw(N + 1), Midx(N + 1), ccw(N);
 //	lint sum = 0;
-//	for (int i = 0; i < N; i++) {
+//	for (int i = 1; i <= N; i++) {
 //		sum += v[i];
-//		cw[i] = sum - x[i];
+//		rcw[i] = sum - x[i];
+//	}
+//	for (int i = 1; i <= N; i++) {
+//		if (rcw[i] <= rcw[i - 1]) {
+//			rcw[i] = rcw[i - 1];
+//			Midx[i] = Midx[i - 1];
+//		}
+//		else {
+//			Midx[i] = i;
+//		}
 //	}
 //
 //	sum = 0;
+//
 //	for (int i = N - 1; i >= 0; i--) {
-//		sum += v[i];
-//		ccw[i] = sum - (C - x[i]);
+//		sum += v[i + 1];
+//		ccw[i] = sum - (C - x[i + 1]);
 //	}
 //
-//	vector<lint> ans(N);
+//	lint ans = rcw[N];
 //	for (int i = 0; i < N; i++) {
-//		ans[i] = max({ 0LL, cw[i], ccw[i] });
-//		for (int j = i + 1; j < N; j++) {
-//			ans[i] = max(ans[i], cw[i] + ccw[j] - min(x[i], (C - x[j])));
-//		}
+//		ans = max(ans, rcw[i] + ccw[i] - min(x[Midx[i]], (C - x[i + 1])));
 //	}
-//	cout << *max_element(ans.begin(), ans.end()) << "\n";
+//
+//	cout << ans << "\n";
 //
 //	return 0;
 //}
