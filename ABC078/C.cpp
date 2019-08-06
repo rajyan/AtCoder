e9906984291ba9dc47716f78faae096761042a04
kitakita7617 <kitakita7617@gmail.com>
2019-08-06 17:56:56 +0900

    78-C

--- ABC078/C.cpp
+++ ABC078/C.cpp
@@ -1,27 +1,42 @@
-#include <cstdio>
-#include <cmath>
-#include <iostream>
-#include <sstream>
-#include <string>
-#include <vector>
-#include <map>
-#include <queue>
-#include <algorithm>
-
-#ifdef _DEBUG
-#define DMP(x) cerr << #x << ": " << x << "\n"
-#else
-#define DMP(x) ((void)0)
-#endif
-
-const int MOD = 1000000007, INF = 1111111111; 
-using namespace std;
-typedef long long lint;
-
-int main() {
-
-	cin.tie(nullptr);
-	ios::sync_with_stdio(false);
-
-	return 0;
-}
+//#include <cstdio>
+//#include <cmath>
+//#include <iostream>
+//#include <sstream>
+//#include <string>
+//#include <vector>
+//#include <map>
+//#include <queue>
+//#include <algorithm>
+//
+//#ifdef _DEBUG
+//#define DMP(x) cerr << #x << ": " << x << "\n"
+//#else
+//#define DMP(x) ((void)0)
+//#endif
+//
+//const int MOD = 1000000007, INF = 1111111111;
+//using namespace std;
+//typedef long long lint;
+//
+//lint mypow(lint a, lint n) {
+//	lint res = 1;
+//	while (n > 0) {
+//		if (n & 1) res = res * a;
+//		a = a * a;
+//		n >>= 1;
+//	}
+//	return res;
+//}
+//
+//int main() {
+//
+//	cin.tie(nullptr);
+//	ios::sync_with_stdio(false);
+//
+//	int N, M;
+//	cin >> N >> M;
+//
+//	cout << ((N - M) * 100 + 1900 * M) * mypow(2, M) << "\n";
+//
+//	return 0;
+//}
