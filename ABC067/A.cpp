74c74a44fc408c3666df11b106384dd575d551aa
kitakita7617 <kitakita7617@gmail.com>
2019-08-16 11:29:52 +0900

    ABC67

---
+++ ABC067/A.cpp
@@ -0,0 +1,54 @@
+//#include <cassert>
+//#include <cstdio>
+//#include <cmath>
+//#include <iostream>
+//#include <sstream>
+//#include <string>
+//#include <vector>
+//#include <map>
+//#include <queue>
+//#include <algorithm>
+//
+//const int MOD = 1000000007, INF = 1111111111;
+//using namespace std;
+//using lint = long long;
+//
+//template <class T>
+//ostream &operator<<(ostream &os, const vector<T> &vec) {
+//	for (int i = 0; i < (int)vec.size(); i++) {
+//		os << vec[i] << (i + 1 == vec.size() ? "" : " ");
+//	}
+//	return os;
+//}
+//
+//#ifdef _DEBUG
+//template <class Head>
+//void dump(const char* str, Head &&h) { cerr << str << " = " << h << "\n"; };
+//template <class Head, class... Tail>
+//void dump(const char* str, Head &&h, Tail &&... t) {
+//	while (*str != ',') cerr << *str++; cerr << " = " << h << "\n";
+//	dump(str + 1, t...);
+//}
+//#define DMP(...) dump(#__VA_ARGS__, __VA_ARGS__)
+//#else 
+//#define DMP(...) ((void)0)
+//#endif
+//
+//int main() {
+//
+//	cin.tie(nullptr);
+//	ios::sync_with_stdio(false);
+//
+//	int A, B;
+//	cin >> A >> B;
+//
+//	bool pos = false;
+//	pos |= A % 3 == 0;
+//	pos |= B % 3 == 0;
+//	pos |= (A + B) % 3 == 0;
+//
+//	if (pos) cout << "Possible" << "\n";
+//	else cout << "Impossible" << "\n";
+//
+//	return 0;
+//}
---
+++ ABC067/C.cpp
@@ -0,0 +1,88 @@
+//#include <cassert>
+//#include <cstdio>
+//#include <cmath>
+//#include <iostream>
+//#include <sstream>
+//#include <string>
+//#include <vector>
+//#include <map>
+//#include <queue>
+//#include <algorithm>
+//
+//const int MOD = 1000000007, INF = 1111111111;
+//using namespace std;
+//using lint = long long;
+//
+//template <class T>
+//ostream &operator<<(ostream &os, const vector<T> &vec) {
+//	for (int i = 0; i < (int)vec.size(); i++) {
+//		os << vec[i] << (i + 1 == vec.size() ? "" : " ");
+//	}
+//	return os;
+//}
+//
+//#ifdef _DEBUG
+//template <class Head>
+//void dump(const char* str, Head &&h) { cerr << str << " = " << h << "\n"; };
+//template <class Head, class... Tail>
+//void dump(const char* str, Head &&h, Tail &&... t) {
+//	while (*str != ',') cerr << *str++; cerr << " = " << h << "\n";
+//	dump(str + 1, t...);
+//}
+//#define DMP(...) dump(#__VA_ARGS__, __VA_ARGS__)
+//#else 
+//#define DMP(...) ((void)0)
+//#endif
+//
+//template <class T>
+//class CuSum {
+//private:
+//	vector<T> cusum;
+//public:
+//	// Constructors 
+//	CuSum(vector<T> const &vec) : cusum(vec.size() + 1) {
+//		for (int i = 0; i < (int)vec.size(); i++) cusum[i + 1] = cusum[i] + vec[i];
+//	}
+//	CuSum(int N) : cusum(N + 1) {}
+//	void add(int i, T x) { cusum[i + 1] += x; }
+//	void init() { for (int i = 0; i < (int)cusum.size() - 1; i++) cusum[i + 1] += cusum[i]; }
+//
+//	// partial sum of 0-indexed [l, r) 
+//	T operator()(int l, int r) {
+//		if (r < l || r < 0) {
+//			assert(0);
+//			return T(0);
+//		}
+//		else return cusum[r] - cusum[l];
+//	}
+//	// 0-indexed
+//	T operator[](int i) { return cusum[i + 1]; }
+//	T back() { return cusum.back(); }
+//};
+//
+//template<class T>
+//inline bool chmin(T &a, T b) { return a > b && (a = b, true); }
+//
+//int main() {
+//
+//	cin.tie(nullptr);
+//	ios::sync_with_stdio(false);
+//
+//	int N;
+//	cin >> N;
+//
+//	vector<lint> a(N);
+//	for (int i = 0; i < N; i++) cin >> a[i];
+//
+//	CuSum<lint> suma(a);
+//	
+//	lint ans = (1LL << 60);
+//	for (int i = 1; i < N; i++) {
+//		DMP(suma(0, 1), suma(i, N));
+//		chmin(ans, abs(suma(0, i) - suma(i, N)));
+//	}
+//
+//	cout << ans << "\n";
+//
+//	return 0;
+//}
