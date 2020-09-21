4314b46a5ed161cb224d1294c3fa0d74d999273d
rajyan <kitakita7617@gmail.com>
2020-09-21 17:37:10 +0900

    added 178

---
+++ ABC178/B.cpp
@@ -0,0 +1,49 @@
+//#ifdef _DEBUG
+//#include "../../library/library/debug_template.cpp"
+//#define DMP(...) dump(#__VA_ARGS__, __VA_ARGS__)
+//#else
+//#define DMP(...) ((void)0)
+//#endif
+//
+//#include <cassert>
+//#include <cstdio>
+//#include <cmath>
+//#include <iostream>
+//#include <iomanip>
+//#include <vector>
+//#include <set>
+//#include <map>
+//#include <queue>
+//#include <numeric>
+//#include <algorithm>
+//
+//using namespace std;
+//using lint = long long;
+//constexpr int MOD = 1000000007, INF = 1010101010;
+//constexpr lint LINF = 1LL << 60;
+//
+//struct init {
+//	init() {
+//		cin.tie(nullptr); ios::sync_with_stdio(false);
+//		cout << fixed << setprecision(10);
+//	}
+//} init_;
+//
+//template<class T>
+//inline bool chmax(T& a, const T b) { return a < b && (a = b, true); }
+//
+//int main() {
+//
+//	lint a, b, c, d;
+//	cin >> a >> b >> c >> d;
+//
+//	lint ans = -LINF;
+//	chmax(ans, a * c);
+//	chmax(ans, b * c);
+//	chmax(ans, b * d);
+//	chmax(ans, a * d);
+//
+//	cout << ans << "\n";
+//
+//	return 0;
+//}
