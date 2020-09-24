73378615e7cd74f5749719d7bab4b0b2c55b95c2
rajyan <kitakita7617@gmail.com>
2020-09-24 22:18:16 +0900

    added 178

--- ABC178/E.cpp
+++ ABC178/E.cpp
@@ -1,37 +1,55 @@
-#ifdef _DEBUG
-#include "../../library/library/debug_template.cpp"
-#define DMP(...) dump(#__VA_ARGS__, __VA_ARGS__)
-#else
-#define DMP(...) ((void)0)
-#endif
-
-#include <cassert>
-#include <cstdio>
-#include <cmath>
-#include <iostream>
-#include <iomanip>
-#include <vector>
-#include <set>
-#include <map>
-#include <queue>
-#include <numeric>
-#include <algorithm>
-
-using namespace std;
-using lint = long long;
-constexpr int MOD = 1000000007, INF = 1010101010;
-constexpr lint LINF = 1LL << 60;
-
-struct init {
-	init() {
-		cin.tie(nullptr); ios::sync_with_stdio(false);
-		cout << fixed << setprecision(10);
-	}
-} init_;
-
-int main() {
-
-
-
-	return 0;
-}
+//#ifdef _DEBUG
+//#include "../../library/library/debug_template.cpp"
+//#define DMP(...) dump(#__VA_ARGS__, __VA_ARGS__)
+//#else
+//#define DMP(...) ((void)0)
+//#endif
+//
+//#include <cassert>
+//#include <cstdio>
+//#include <cmath>
+//#include <iostream>
+//#include <iomanip>
+//#include <vector>
+//#include <set>
+//#include <map>
+//#include <queue>
+//#include <numeric>
+//#include <algorithm>
+//
+//using namespace std;
+//using lint = long long;
+//constexpr int MOD = 1000000007, INF = 1010101010;
+//constexpr lint LINF = 1LL << 60;
+//
+//struct init {
+//	init() {
+//		cin.tie(nullptr); ios::sync_with_stdio(false);
+//		cout << fixed << setprecision(10);
+//	}
+//} init_;
+//
+//template<class T>
+//inline bool chmin(T& a, const T b) { return a > b && (a = b, true); }
+//template<class T>
+//inline bool chmax(T& a, const T b) { return a < b && (a = b, true); }
+//
+//int main() {
+//
+//	int N;
+//	cin >> N;
+//
+//	lint umax = -INF, umin = INF, vmax = -INF, vmin = INF;
+//	for (int i = 0; i < N; i++) {
+//		lint x, y;
+//		cin >> x >> y;
+//		chmax(umax, x + y);
+//		chmin(umin, x + y);
+//		chmax(vmax, x - y);
+//		chmin(vmin, x - y);
+//	}
+//
+//	cout << max(umax - umin, vmax - vmin) << "\n";
+//
+//	return 0;
+//}
--- ABC178/F.cpp
+++ ABC178/F.cpp
@@ -1,37 +1,58 @@
-//#ifdef _DEBUG
-//#include "../../library/library/debug_template.cpp"
-//#define DMP(...) dump(#__VA_ARGS__, __VA_ARGS__)
-//#else
-//#define DMP(...) ((void)0)
-//#endif
-//
-//#include <cassert>
-//#include <cstdio>
-//#include <cmath>
-//#include <iostream>
-//#include <iomanip>
-//#include <vector>
-//#include <set>
-//#include <map>
-//#include <queue>
-//#include <numeric>
-//#include <algorithm>
-//
-//using namespace std;
-//using lint = long long;
-//constexpr int MOD = 1000000007, INF = 1010101010;
-//constexpr lint LINF = 1LL << 60;
-//
-//struct init {
-//	init() {
-//		cin.tie(nullptr); ios::sync_with_stdio(false);
-//		cout << fixed << setprecision(10);
-//	}
-//} init_;
-//
-//int main() {
-//
-//
-//
-//	return 0;
-//}
+#ifdef _DEBUG
+#include "../../library/library/debug_template.cpp"
+#define DMP(...) dump(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define DMP(...) ((void)0)
+#endif
+
+#include <cassert>
+#include <cstdio>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <numeric>
+#include <algorithm>
+
+using namespace std;
+using lint = long long;
+constexpr int MOD = 1000000007, INF = 1010101010;
+constexpr lint LINF = 1LL << 60;
+
+struct init {
+	init() {
+		cin.tie(nullptr); ios::sync_with_stdio(false);
+		cout << fixed << setprecision(10);
+	}
+} init_;
+
+int main() {
+
+	int N;
+	cin >> N;
+
+	vector<int> cnt(N + 1);
+	vector<int> A(N), B(N);
+
+	for (int i = 0; i < N; i++) {
+		cin >> A[i];
+		cnt[A[i]]++;
+	}	
+	for (int i = 0; i < N; i++) {
+		cin >> B[i];
+		cnt[B[i]]++;
+	}
+
+	if (any_of(cnt.begin(), cnt.end(), [&N](auto c) { return c > N; })) {
+		cout << "No" << "\n";
+		return 0;
+	}
+
+	cout << "Yes" << "\n";
+	for (const auto& e : ans) cout << e << " ";
+
+	return 0;
+}
