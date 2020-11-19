64c1ae7e8fe2c217b3e90b7eaaa30d8d81548140
rajyan <kitakita7617@gmail.com>
2020-11-19 22:07:38 +0900

    ARC107-D

---
+++ ARC107/A.cpp
@@ -0,0 +1,175 @@
+//#ifdef _DEBUG
+//#include "../../library/library/debug_template.cpp"
+//#define DMP(...) dump(#__VA_ARGS__, __VA_ARGS__)
+//#else
+//#define DMP(...) ((void)0)
+//#endif
+//
+//#include <cassert>
+//#include <cstdio>
+//#include <cmath>
+//#include <iostream>
+//#include <iomanip>
+//#include <vector>
+//#include <set>
+//#include <map>
+//#include <queue>
+//#include <numeric>
+//#include <algorithm>
+//
+//using namespace std;
+//using lint = long long;
+//constexpr int MOD = 998244353, INF = 1010101010;
+//constexpr lint LINF = 1LL << 60;
+//
+//struct init {
+//	init() {
+//		cin.tie(nullptr); ios::sync_with_stdio(false);
+//		cout << fixed << setprecision(10);
+//	}
+//} init_;
+//
+//class UnionFind {
+//private:
+//	vector<int> data;
+//public:
+//	UnionFind(int size) : data(size, -1) { }
+//	int root(int x) { return data[x] < 0 ? x : data[x] = root(data[x]); }
+//	bool is_same(int x, int y) { return root(x) == root(y); }
+//	int size(int x) { return -data[root(x)]; }
+//
+//	int get_data(int x) { return data[x]; }
+//
+//	bool unify(int x, int y) {
+//		x = root(x); y = root(y);
+//		if (x != y) {
+//			if (data[y] < data[x]) swap(x, y);
+//			data[x] += data[y]; data[y] = x;
+//			return true;
+//		}
+//		return false;
+//	}
+//};
+//
+//template<class T>
+//vector<T> make_vec(size_t s, T val) { return vector<T>(s, val); }
+//template<class... Size>
+//auto make_vec(size_t s, Size... tail) {
+//	return vector<decltype(make_vec(tail...))>(s, make_vec(tail...));
+//}
+//
+//template<int Modulo = MOD>
+//struct Mint {
+//
+//	lint val;
+//	constexpr Mint(lint v = 0) noexcept : val(v% Modulo) { if (val < 0) val += Modulo; }
+//
+//	constexpr Mint& operator+=(const Mint& r) noexcept {
+//		val += r.val;
+//		if (val >= Modulo) val -= Modulo;
+//		return *this;
+//	}
+//	constexpr Mint& operator-=(const Mint& r) noexcept {
+//		val -= r.val;
+//		if (val < 0) val += Modulo;
+//		return *this;
+//	}
+//	constexpr Mint& operator*=(const Mint& r) noexcept {
+//		val = val * r.val % Modulo;
+//		return *this;
+//	}
+//	constexpr Mint& operator/=(const Mint& r) noexcept {
+//		lint a = r.val, b = Modulo, u = 1, v = 0;
+//		while (b) {
+//			lint t = a / b;
+//			a -= t * b; swap(a, b);
+//			u -= t * v; swap(u, v);
+//		}
+//		val = val * u % Modulo;
+//		if (val < 0) val += Modulo;
+//		return *this;
+//	}
+//
+//	constexpr Mint operator+(const Mint& r) const noexcept { return Mint(*this) += r; }
+//	constexpr Mint operator-(const Mint& r) const noexcept { return Mint(*this) -= r; }
+//	constexpr Mint operator*(const Mint& r) const noexcept { return Mint(*this) *= r; }
+//	constexpr Mint operator/(const Mint& r) const noexcept { return Mint(*this) /= r; }
+//
+//	constexpr Mint operator-() const noexcept { return val ? Modulo - val : 0; }
+//
+//	constexpr bool operator==(const Mint& r) const noexcept { return val == r.val; }
+//	constexpr bool operator!=(const Mint& r) const noexcept { return !((*this) == r); }
+//	constexpr bool operator< (const Mint& r) const noexcept { return val < r.val; }
+//
+//	friend ostream& operator << (ostream& os, const Mint<Modulo>& x) noexcept { return os << x.val; }
+//	friend istream& operator >> (istream& is, Mint<Modulo>& x) noexcept {
+//		lint tmp; is >> tmp;
+//		x = Mint(tmp);
+//		return is;
+//	}
+//
+//	constexpr Mint pow(lint n) const noexcept {
+//		Mint res = 1, tmp = val;
+//		while (n > 0) {
+//			if (n & 1) res *= tmp;
+//			tmp *= tmp;
+//			n >>= 1;
+//		}
+//		return res;
+//	}
+//};
+//
+//using mint = Mint<>;
+//
+//
+//int main() {
+//
+//	int N, K;
+//	cin >> N >> K;
+//
+//	auto grid = make_vec(N, N, 0);
+//	for (int i = 0; i < N; i++) for (int j = 0; j < N; j++) cin >> grid[i][j];
+//
+//	UnionFind col(N);
+//	for (int i = 0; i < N; i++) {
+//		for (int j = i + 1; j < N; j++) {
+//			bool all = true;
+//			for (int k = 0; k < N; k++) {
+//				if (grid[i][k] + grid[j][k] > K) all = false;
+//			}
+//			if (all) {
+//				col.unify(i, j);
+//			}
+//		}
+//	}
+//
+//	UnionFind row(N);
+//	for (int i = 0; i < N; i++) {
+//		for (int j = i + 1; j < N; j++) {
+//			bool all = true;
+//			for (int k = 0; k < N; k++) {
+//				if (grid[k][i] + grid[k][j] > K) all = false;
+//			}
+//			if (all) {
+//				row.unify(i, j);
+//			}
+//		}
+//	}
+//
+//	mint ans = 1;
+//	for (int i = 0; i < N; i++) {
+//		int size = -row.get_data(i);
+//		if (size <= 0) continue;
+//		for (int j = 1; j <= size; j++) ans *= j;
+//	}
+//
+//	for (int i = 0; i < N; i++) {
+//		int size = -col.get_data(i);
+//		if (size <= 0) continue;
+//		for (int j = 1; j <= size; j++) ans *= j;
+//	}
+//
+//	cout << ans << "\n";
+//
+//	return 0;
+//}
