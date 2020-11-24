223c83c1d94db4ac37bbfaf57cf0aacd65dea6e8
rajyan <kitakita7617@gmail.com>
2020-11-24 22:57:32 +0900

    cyan

--- ABC181/C.cpp
+++ ABC181/C.cpp
@@ -1,87 +1,87 @@
-#ifdef _DEBUG
-#include "../../library/library/debug_template.cpp"
-#define DMP(...) dump(#__VA_ARGS__, __VA_ARGS__)
-#else
-#define DMP(...) ((void)0)
-#endif
-
-#include <cassert>
-#include <cstdio>
-#include <cmath>
-#include <iostream>
-#include <iomanip>
-#include <vector>
-#include <set>
-#include <map>
-#include <queue>
-#include <numeric>
-#include <algorithm>
-
-using namespace std;
-using lint = long long;
-constexpr int MOD = 1000000007, INF = 1010101010;
-constexpr lint LINF = 1LL << 60;
-
-struct init {
-	init() {
-		cin.tie(nullptr); ios::sync_with_stdio(false);
-		cout << fixed << setprecision(10);
-	}
-} init_;
-
-template<class T>
-struct Point2D {
-	T x, y;
-	constexpr Point2D(T x = 0, T y = 0) noexcept : x(x), y(y) {};
-	constexpr Point2D(pair<T, T>& p) noexcept : x(p.first), y(p.second) {};
-	constexpr bool operator==(const Point2D& rhs) const noexcept { return x == rhs.x && y == rhs.y; }
-	constexpr bool operator!=(const Point2D& rhs) const noexcept { return !((*this) == rhs); }
-	constexpr bool operator< (const Point2D& rhs) const noexcept { return x < rhs.x || (x == rhs.x && y < rhs.y); }
-	constexpr bool operator> (const Point2D& rhs) const noexcept { return rhs < (*this); }
-	constexpr bool operator<=(const Point2D& rhs) const noexcept { return !((*this) > rhs); }
-	constexpr bool operator>=(const Point2D& rhs) const noexcept { return !((*this) < rhs); }
-	constexpr Point2D  operator+ (const Point2D& rhs) const noexcept { return { x + rhs.x, y + rhs.y }; }
-	constexpr Point2D  operator- (const Point2D& rhs) const noexcept { return { x - rhs.x, y - rhs.y }; }
-	constexpr Point2D  operator* (const T& k) const noexcept { return { k * x, k * y }; }
-	constexpr Point2D  operator/ (const T& k) const noexcept { return { x / k, y / k }; }
-	constexpr Point2D& operator+=(const Point2D& rhs) noexcept { return ((*this) = (*this) + rhs); }
-	constexpr Point2D& operator-=(const Point2D& rhs) noexcept { return ((*this) = (*this) - rhs); }
-	constexpr Point2D& operator*=(const T& k) noexcept { return ((*this) = (*this) * k); }
-	constexpr Point2D& operator/=(const T& k) noexcept { return ((*this) = (*this) / k); }
-	constexpr Point2D& operator--(int) noexcept { return ((*this) -= Point2D(1, 1)); };
-	constexpr Point2D& operator++(int) noexcept { return ((*this) += Point2D(1, 1)); };
-	constexpr Point2D  operator- () const noexcept { return { -x, -y }; }
-	constexpr T operator* (const Point2D& rhs) const noexcept { return x * rhs.x + y * rhs.y; }
-	constexpr Point2D nor() const noexcept { return { y, -x }; }
-	constexpr T pow() const noexcept { return x * x + y * y; }
-	friend istream& operator>>(istream& is, Point2D& p) { return is >> p.x >> p.y; }
-	friend ostream& operator<<(ostream& os, const Point2D& p) { return os << p.x << " " << p.y; }
-	template<class U> U& operator[](vector<vector<U>>& v) { return v[x][y]; }
-};
-
-using pnt = Point2D<lint>;
-
-int main() {
-
-	int N;
-	cin >> N;
-
-	vector<pnt> xy(N);
-	for (int i = 0; i < N; i++) cin >> xy[i];
-
-	bool exist = false;
-	for (int i = 0; i < N; i++) {
-		for (int j = i + 1; j < N; j++) {
-			for (int k = j + 1; k < N; k++) {
-				pnt p = xy[i] - xy[j];
-				pnt q = xy[i] - xy[k];
-				if (1LL * (p * q) * (p * q) == 1LL * p.pow() * q.pow()) exist = true;
-			}
-		}
-	}
-
-	if (exist) cout << "Yes" << "\n";
-	else cout << "No" << "\n";
-
-	return 0;
-}
+//#ifdef _DEBUG
+//#include "../../library/library/debug_template.cpp"
+//#define DMP(...) dump(#__VA_ARGS__, __VA_ARGS__)
+//#else
+//#define DMP(...) ((void)0)
+//#endif
+//
+//#include <cassert>
+//#include <cstdio>
+//#include <cmath>
+//#include <iostream>
+//#include <iomanip>
+//#include <vector>
+//#include <set>
+//#include <map>
+//#include <queue>
+//#include <numeric>
+//#include <algorithm>
+//
+//using namespace std;
+//using lint = long long;
+//constexpr int MOD = 1000000007, INF = 1010101010;
+//constexpr lint LINF = 1LL << 60;
+//
+//struct init {
+//	init() {
+//		cin.tie(nullptr); ios::sync_with_stdio(false);
+//		cout << fixed << setprecision(10);
+//	}
+//} init_;
+//
+//template<class T>
+//struct Point2D {
+//	T x, y;
+//	constexpr Point2D(T x = 0, T y = 0) noexcept : x(x), y(y) {};
+//	constexpr Point2D(pair<T, T>& p) noexcept : x(p.first), y(p.second) {};
+//	constexpr bool operator==(const Point2D& rhs) const noexcept { return x == rhs.x && y == rhs.y; }
+//	constexpr bool operator!=(const Point2D& rhs) const noexcept { return !((*this) == rhs); }
+//	constexpr bool operator< (const Point2D& rhs) const noexcept { return x < rhs.x || (x == rhs.x && y < rhs.y); }
+//	constexpr bool operator> (const Point2D& rhs) const noexcept { return rhs < (*this); }
+//	constexpr bool operator<=(const Point2D& rhs) const noexcept { return !((*this) > rhs); }
+//	constexpr bool operator>=(const Point2D& rhs) const noexcept { return !((*this) < rhs); }
+//	constexpr Point2D  operator+ (const Point2D& rhs) const noexcept { return { x + rhs.x, y + rhs.y }; }
+//	constexpr Point2D  operator- (const Point2D& rhs) const noexcept { return { x - rhs.x, y - rhs.y }; }
+//	constexpr Point2D  operator* (const T& k) const noexcept { return { k * x, k * y }; }
+//	constexpr Point2D  operator/ (const T& k) const noexcept { return { x / k, y / k }; }
+//	constexpr Point2D& operator+=(const Point2D& rhs) noexcept { return ((*this) = (*this) + rhs); }
+//	constexpr Point2D& operator-=(const Point2D& rhs) noexcept { return ((*this) = (*this) - rhs); }
+//	constexpr Point2D& operator*=(const T& k) noexcept { return ((*this) = (*this) * k); }
+//	constexpr Point2D& operator/=(const T& k) noexcept { return ((*this) = (*this) / k); }
+//	constexpr Point2D& operator--(int) noexcept { return ((*this) -= Point2D(1, 1)); };
+//	constexpr Point2D& operator++(int) noexcept { return ((*this) += Point2D(1, 1)); };
+//	constexpr Point2D  operator- () const noexcept { return { -x, -y }; }
+//	constexpr T operator* (const Point2D& rhs) const noexcept { return x * rhs.x + y * rhs.y; }
+//	constexpr Point2D nor() const noexcept { return { y, -x }; }
+//	constexpr T pow() const noexcept { return x * x + y * y; }
+//	friend istream& operator>>(istream& is, Point2D& p) { return is >> p.x >> p.y; }
+//	friend ostream& operator<<(ostream& os, const Point2D& p) { return os << p.x << " " << p.y; }
+//	template<class U> U& operator[](vector<vector<U>>& v) { return v[x][y]; }
+//};
+//
+//using pnt = Point2D<lint>;
+//
+//int main() {
+//
+//	int N;
+//	cin >> N;
+//
+//	vector<pnt> xy(N);
+//	for (int i = 0; i < N; i++) cin >> xy[i];
+//
+//	bool exist = false;
+//	for (int i = 0; i < N; i++) {
+//		for (int j = i + 1; j < N; j++) {
+//			for (int k = j + 1; k < N; k++) {
+//				pnt p = xy[i] - xy[j];
+//				pnt q = xy[i] - xy[k];
+//				if (1LL * (p * q) * (p * q) == 1LL * p.pow() * q.pow()) exist = true;
+//			}
+//		}
+//	}
+//
+//	if (exist) cout << "Yes" << "\n";
+//	else cout << "No" << "\n";
+//
+//	return 0;
+//}
---
+++ cyan/ARC108-C.cpp
@@ -0,0 +1,96 @@
+//#ifdef _DEBUG
+//#include "../../library/library/debug_template.cpp"
+//#define DMP(...) dump(#__VA_ARGS__, __VA_ARGS__)
+//#else
+//#define DMP(...) ((void)0)
+//#endif
+//
+//#include <cassert>
+//#include <cstdio>
+//#include <cmath>
+//#include <iostream>
+//#include <iomanip>
+//#include <vector>
+//#include <set>
+//#include <map>
+//#include <unordered_map>
+//#include <queue>
+//#include <numeric>
+//#include <algorithm>
+//#include <bitset>
+//
+//using namespace std;
+//using lint = long long;
+//constexpr int MOD = 1000000007, INF = 1010101010;
+//constexpr lint LINF = 1LL << 60;
+//
+//struct init {
+//	init() {
+//		cin.tie(nullptr); ios::sync_with_stdio(false);
+//		cout << fixed << setprecision(10);
+//	}
+//} init_;
+//
+//template<class T = lint>
+//struct Edge {
+//	int to;
+//	T cost;
+//	Edge() {}
+//	Edge(int to, T cost = 1) : to(to), cost(cost) {}
+//	bool operator>(const Edge& r) const { return this->cost > r.cost; }
+//};
+//
+//int main() {
+//
+//	int N, M;
+//	cin >> N >> M;
+//
+//	vector<vector<Edge<int>>> edges(N);
+//	for (int i = 0; i < M; i++) {
+//		int u, v, len;
+//		cin >> u >> v >> len;
+//		u--, v--;
+//		edges[u].emplace_back(v, len);
+//		edges[v].emplace_back(u, len);
+//	}
+//
+//	vector<int> ans(N, -1);
+//	queue<pair<int, Edge<int>>> que;
+//	que.emplace(0, Edge<int>(0, 0));
+//	while (!que.empty()) {
+//
+//		const auto [now, edge] = que.front();
+//		que.pop();
+//		if (ans[now] != -1) continue;
+//
+//		const auto &[prev, p_cost] = edge;
+//		if (ans[prev] != p_cost) ans[now] = p_cost;
+//		else ans[now] = 0;
+//
+//		for (auto [next, cost] : edges[now]) {
+//			if (ans[next] != -1) continue;
+//			que.emplace(next, Edge<int>(now, cost));
+//		}
+//	}
+//	
+//	vector<int> used(N + 1);
+//	for (int i = 0; i < N; i++) {
+//		used[ans[i]] = 1;
+//	}
+//
+//	int not_used = -1;
+//	for (int i = 0; i <= N; i++) {
+//		if (!used[i]) {
+//			not_used = i;
+//			break;
+//		}
+//	}
+//
+//	for (int i = 0; i < N; i++) {
+//		if (ans[i] == 0) ans[i] = not_used;
+//	}
+//
+//	for (const auto& e : ans) cout << e << "\n";
+//
+//	return 0;
+//}
