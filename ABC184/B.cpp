cf85755a1429a5bc01052f02394c380bb198d4aa
rajyan <kitakita7617@gmail.com>
2020-11-28 18:05:31 +0900

    ABC184-D

--- ABC184/B.cpp
+++ ABC184/B.cpp
@@ -1,43 +1,43 @@
-#ifdef _DEBUG
-#include "../../library/library/debug_template.cpp"
-#define DMP(...) dump(#__VA_ARGS__, __VA_ARGS__)
-#else
-#define DMP(...) ((void)0)
-#endif
-
-#include <cassert>
-#include <cstdio>
-#include <cmath>
-#include <iostream>
-#include <iomanip>
-#include <vector>
-#include <set>
-#include <map>
-#include <queue>
-#include <numeric>
-#include <algorithm>
-
-using namespace std;
-using lint = long long;
-constexpr int MOD = 1000000007, INF = 1010101010;
-constexpr lint LINF = 1LL << 60;
-
-struct init {
-	init() {
-		cin.tie(nullptr); ios::sync_with_stdio(false);
-		cout << fixed << setprecision(10);
-	}
-} init_;
-
-int main() {
-
-	int N, X;
-	cin >> N >> X;
-	string S;
-	cin >> S;
-
-	for (const auto& c : S) X = max(0, X + (c == 'o') * 2 - 1);
-	cout << X << "\n";
-
-	return 0;
-}
+//#ifdef _DEBUG
+//#include "../../library/library/debug_template.cpp"
+//#define DMP(...) dump(#__VA_ARGS__, __VA_ARGS__)
+//#else
+//#define DMP(...) ((void)0)
+//#endif
+//
+//#include <cassert>
+//#include <cstdio>
+//#include <cmath>
+//#include <iostream>
+//#include <iomanip>
+//#include <vector>
+//#include <set>
+//#include <map>
+//#include <queue>
+//#include <numeric>
+//#include <algorithm>
+//
+//using namespace std;
+//using lint = long long;
+//constexpr int MOD = 1000000007, INF = 1010101010;
+//constexpr lint LINF = 1LL << 60;
+//
+//struct init {
+//	init() {
+//		cin.tie(nullptr); ios::sync_with_stdio(false);
+//		cout << fixed << setprecision(10);
+//	}
+//} init_;
+//
+//int main() {
+//
+//	int N, X;
+//	cin >> N >> X;
+//	string S;
+//	cin >> S;
+//
+//	for (const auto& c : S) X = max(0, X + (c == 'o') * 2 - 1);
+//	cout << X << "\n";
+//
+//	return 0;
+//}
