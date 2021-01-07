961147f7a79c9c7b69ebc0941e465ecdb1fc6771
rajyan <kitakita7617@gmail.com>
2021-01-07 09:10:48 +0900

    ABC185-D

--- ABC/ABC185/D.cpp
+++ ABC/ABC185/D.cpp
@@ -1,6 +1,6 @@
 
 #ifdef _DEBUG
-#include "../../src/library/debug_template.cpp"
+#include "../../../library/src/debug_template.cpp"
 #define DMP(...) dump(#__VA_ARGS__, __VA_ARGS__)
 #else
 #define DMP(...) ((void)0)
@@ -35,8 +35,42 @@ struct init {
     }
 } init_;
 
+template<class T>
+inline bool chmax(T &a, const T b) { return a < b && (a = b, true); }
+
+template<class T>
+inline bool chmin(T &a, const T b) { return a > b && (a = b, true); }
+
 int main() {
 
+    int N, M;
+    cin >> N >> M;
+
+    vector<int> A(M);
+    for (int i = 0; i < M; i++) cin >> A[i];
+    A.emplace_back(0);
+    A.emplace_back(N + 1);
+    sort(A.begin(), A.end());
+
+    vector<int> sub(M + 1);
+    int k = INF;
+    for (int i = 0; i <= M; i++) {
+        sub[i] = A[i + 1] - A[i] - 1;
+        if (sub[i] != 0) {
+            chmin(k, sub[i]);
+        }
+    }
+
+    DMP(sub, k);
+
+    lint ans = 0;
+    for (int i = 0; i <= M; i++) {
+        if (sub[i] != 0){
+            ans += (sub[i] - 1) / k + 1;
+        }
+    }
+
+    cout << ans << '\n';
 
     return 0;
 }
