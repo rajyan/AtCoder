83711b59a61d3f8e1643b047c6bcad470e36ae39
rajyan <kitakita7617@gmail.com>
2021-01-09 17:52:29 +0900

    ABC185-E

--- ABC/ABC185/E.cpp
+++ ABC/ABC185/E.cpp
@@ -37,6 +37,26 @@ struct init {
 
 int main() {
 
+    int N, M;
+    cin >> N >> M;
+
+    vector<int> A(N), B(M);
+    for (int i = 0; i < N; i++) cin >> A[i];
+    for (int i = 0; i < M; i++) cin >> B[i];
+
+    vector<vector<int>> dp(N + 1, vector<int>(M + 1, INF));
+    for (int i = 0; i <= N; i++) dp[i][0] = i;
+    for (int j = 0; j <= M; j++) dp[0][j] = j;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            dp[i + 1][j + 1] = min({dp[i][j] + (A[i] != B[j]), dp[i + 1][j] + 1, dp[i][j + 1] + 1});
+        }
+    }
+
+    DMP(dp);
+
+    cout << dp[N][M] << '\n';
 
     return 0;
 }
