4d9d845f65c5deb8fc0a092bbfd9315ddccc8321
rajyan <kitakita7617@gmail.com>
2021-01-25 22:11:39 +0900

    fixed ABC188-F

--- ABC/ABC189/F.cpp
+++ ABC/ABC189/F.cpp
@@ -1,4 +1,3 @@
-
 #ifdef _DEBUG
 #include "../../../library/src/debug_template.cpp"
 #define DMP(...) dump(#__VA_ARGS__, __VA_ARGS__)
@@ -35,55 +34,7 @@ struct init {
     }
 } init_;
 
-inline int clz(lint x) {
-    union {
-        unsigned long long as_uint64;
-        double as_double;
-    } data{};
-    data.as_double = (double)x + 0.5;
-    int n = 1054 - (int)(data.as_uint64 >> 52);
-    return 32 + n;
-}
-
-template<class T>
-inline bool chmin(T &a, const T b) { return a > b && (a = b, true); }
-
 int main() {
-
-    lint X, Y;
-    cin >> X >> Y;
-
-    if (X >= Y) {
-        cout << X - Y << '\n';
-        return 0;
-    }
-    int ans = INF;
-    int s = clz(X) - clz(Y);
-
-    auto asc = [&] (int S) -> int {
-        lint x = X << S;
-        lint y = Y;
-        DMP(x, y, S);
-        if (x > y) swap(x, y);
-        int bit = 0, res = 0;
-        while (y != x && bit <= S) {
-            if (((y >> bit) ^ (x >> bit)) & 1) {
-                x += (1LL << bit);
-                res++;
-            }
-            bit++;
-        }
-        DMP(res);
-        return res + y - x;
-    };
-
-    DMP(s);
-
-    for (int i = 0; i <= s + 1; i++) {
-        chmin(ans, i + asc(i));
-    }
-
-    cout << ans << '\n';
 
     return 0;
 }
