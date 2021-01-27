bf2426ebb00f6eb5ee5bc1283400ace703332ee0
rajyan <kitakita7617@gmail.com>
2021-01-27 09:25:42 +0900

    add ARC105, solved ARC105-B

---
+++ ARC/ARC105/B.cpp
@@ -0,0 +1,54 @@
+
+#ifdef _DEBUG
+#include "../../../library/src/debug_template.cpp"
+#define DMP(...) dump(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define DMP(...) ((void)0)
+#endif
+
+#include <cassert>
+#include <cstdio>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <numeric>
+#include <algorithm>
+#include <bitset>
+#include <functional>
+
+using namespace std;
+using lint = long long;
+constexpr int MOD = 1000000007, INF = 1010101010;
+constexpr lint LINF = 1LL << 60;
+
+struct init {
+    init() {
+        cin.tie(nullptr);
+        ios::sync_with_stdio(false);
+        cout << fixed << setprecision(10);
+    }
+} init_;
+
+int main() {
+
+    int N;
+    cin >> N;
+
+    vector<int> a(N);
+    for (int i = 0; i < N; i++) cin >> a[i];
+
+    int ans = 0;
+    for (int i = 0; i < N; i++) {
+        ans = gcd(ans, a[i]);
+    }
+
+    cout << ans << '\n';
+
+    return 0;
+}
