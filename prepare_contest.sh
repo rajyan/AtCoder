df3e67ad6c545cc76d3f9dc3ead0e1f7da1b2841
rajyan <kitakita7617@gmail.com>
2021-01-30 14:00:22 +0900

    fixed cpp to hpp

--- prepare_contest.sh
+++ prepare_contest.sh
@@ -13,7 +13,7 @@ file_num=${2:-6}
 i=0
 PROJECT_NAME=$contest_dir envsubst < "$CMAKE_TEMPLATE" > "$PROJECT_DIR"/"$contest_dir"/CMakeLists.txt
 for c in {A..Z} ; do
-  cat "$LIBRARY_DIR"/template.cpp > "$PROJECT_DIR"/"$contest_dir"/"$c".cpp
+  cat "$LIBRARY_DIR"/template.hpp > "$PROJECT_DIR"/"$contest_dir"/"$c".cpp
   echo "add_executable(${contest_dir##*/}_${c} ${c}.cpp)" >> "$PROJECT_DIR"/"$contest_dir"/CMakeLists.txt
   (( ++i == file_num )) && break
 done
