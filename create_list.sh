2501905d50411bc641c8edd98f5d77610e964cf3
rajyan <kitakita7617@gmail.com>
2021-01-31 22:20:50 +0900

    revert create_list

--- create_list.sh
+++ create_list.sh
@@ -8,10 +8,7 @@ CONTEST_DIR=${1?'Set directory name!'}
 CMAKE_FILE="$PROJECT_DIR"/"$CONTEST_DIR"/CMakeLists.txt
 PROJECT_NAME=$CONTEST_DIR envsubst < "$CMAKE_TEMPLATE" > "$CMAKE_FILE"
 
-for file in "$PROJECT_DIR"/"$CONTEST_DIR"/*.[hc]pp ; do
+for file in "$PROJECT_DIR"/"$CONTEST_DIR"/*.cpp ; do
   name=${file##*/}
-  echo "add_executable(${CONTEST_DIR##*/}_${name%.[hc]pp} $name)" >> "$CMAKE_FILE"
-  if [[ $name == *.hpp ]]; then
-    echo "set_target_properties(${CONTEST_DIR##*/}_${name%.hpp} PROPERTIES LINKER_LANGUAGE CXX)" >>"$CMAKE_FILE"
-  fi
+  echo "add_executable(${CONTEST_DIR##*/}_${name%.cpp} $name)" >> "$CMAKE_FILE"
 done
