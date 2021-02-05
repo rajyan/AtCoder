101de56fa87144d2fc5953ecf56e617f50fd0d11
rajyan <kitakita7617@gmail.com>
2021-02-05 09:11:45 +0900

    keywnce2021-C

---
+++ study/cyan/keyence2021-C.cpp
@@ -0,0 +1,145 @@
+#ifdef _DEBUG
+#include "../../../library/src/debug_template.hpp"
+#define DMP(...) dump(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define DMP(...) ((void)0)
+#endif
+
+#include <cassert>
+#include <cstdio>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <numeric>
+#include <algorithm>
+#include <bitset>
+#include <functional>
+
+using namespace std;
+using lint = long long;
+constexpr int INF = 1010101010;
+constexpr lint LINF = 1LL << 60;
+
+struct init {
+    init() {
+        cin.tie(nullptr);
+        ios::sync_with_stdio(false);
+        cout << fixed << setprecision(10);
+    }
+} init_;
+
+template<const int &Modulo>
+struct Mint {
+
+    lint val;
+    constexpr Mint(lint v = 0) noexcept: val(v % Modulo) { if (val < 0) val += Modulo; }
+
+    constexpr Mint &operator+=(const Mint &r) noexcept {
+        val += r.val;
+        if (val >= Modulo) val -= Modulo;
+        return *this;
+    }
+    constexpr Mint &operator-=(const Mint &r) noexcept {
+        val -= r.val;
+        if (val < 0) val += Modulo;
+        return *this;
+    }
+    constexpr Mint &operator*=(const Mint &r) noexcept {
+        val = val * r.val % Modulo;
+        return *this;
+    }
+    constexpr Mint &operator/=(const Mint &r) noexcept {
+        lint a{r.val}, b{Modulo}, u{1}, v{0};
+        while (b) {
+            lint t = a / b;
+            a -= t * b;
+            a ^= b, b ^= a, a ^= b;
+            u -= t * v;
+            u ^= v, v ^= u, u ^= v;
+        }
+        val = val * u % Modulo;
+        if (val < 0) val += Modulo;
+        return *this;
+    }
+
+    constexpr Mint operator+(const Mint &r) const noexcept { return Mint(*this) += r; }
+    constexpr Mint operator-(const Mint &r) const noexcept { return Mint(*this) -= r; }
+    constexpr Mint operator*(const Mint &r) const noexcept { return Mint(*this) *= r; }
+    constexpr Mint operator/(const Mint &r) const noexcept { return Mint(*this) /= r; }
+
+    constexpr Mint operator-() const noexcept { return Mint(-val); }
+
+    constexpr bool operator==(const Mint &r) const noexcept { return val == r.val; }
+    constexpr bool operator!=(const Mint &r) const noexcept { return !((*this) == r); }
+    constexpr bool operator<(const Mint &r) const noexcept { return val < r.val; }
+
+    constexpr friend ostream &operator<<(ostream &os, const Mint<Modulo> &x) noexcept { return os << x.val; }
+    constexpr friend istream &operator>>(istream &is, Mint<Modulo> &x) noexcept {
+        lint tmp{};
+        is >> tmp;
+        x = Mint(tmp);
+        return is;
+    }
+
+    [[nodiscard]] constexpr Mint pow(lint n) const noexcept {
+        Mint res{1}, tmp{*this};
+        while (n > 0) {
+            if (n & 1) res *= tmp;
+            tmp *= tmp;
+            n >>= 1;
+        }
+        return res;
+    }
+};
+
+constexpr int MOD = 998244353;
+using mint = Mint<MOD>;
+
+int RMOD;
+using rmint = Mint<RMOD>;
+
+template<class T>
+vector<T> make_vec(size_t s, T val) { return vector<T>(s, val); }
+template<class... Size>
+auto make_vec(size_t s, Size... tail) {
+    return vector<decltype(make_vec(tail...))>(s, make_vec(tail...));
+}
+
+int main() {
+
+    int H, W, K;
+    cin >> H >> W >> K;
+
+    auto grid = make_vec(H, W, char{});
+    auto dp = make_vec(H, W, mint{});
+    for (int i = 0; i < K; i++) {
+        int h, w;
+        char c;
+        cin >> h >> w >> c;
+        h--, w--;
+        grid[h][w] = c;
+    }
+    DMP(grid);
+
+    dp[0][0] = mint(3).pow(H * W - K);
+    constexpr mint z = mint(2) / 3;
+    for (int i = 0; i < H; i++) {
+        for (int j = 0; j < W; j++) {
+            char c = grid[i][j];
+            mint r = c == char{} ? z : 1;
+            if (c != 'D' && j + 1 < W) dp[i][j + 1] += r * dp[i][j];
+            if (c != 'R' && i + 1 < H) dp[i + 1][j] += r * dp[i][j];
+        }
+    }
+    DMP(dp);
+
+    cout << dp.back().back() << '\n';
+
+    return 0;
+}
