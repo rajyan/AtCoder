f1de4286ca231b2046dc49db8fb328a7c6e1c882
rajyan <kitakita7617@gmail.com>
2021-02-07 12:43:14 +0900

    add boost

--- prepare_contest.sh
+++ prepare_contest.sh
@@ -7,13 +7,14 @@ CMAKE_TEMPLATE=${CMAKE_TEMPLATE:-'/home/rajyan/CLion/AtCoder/CMakeLists_template
 # make directory
 contest_dir=${1?'Set directory name!'}
 mkdir "$PROJECT_DIR"/"$contest_dir"
+cd "$PROJECT_DIR"/"$contest_dir"
 
 # make files and CmakeLists
 file_num=${2:-6}
 i=0
-PROJECT_NAME=$contest_dir envsubst < "$CMAKE_TEMPLATE" > "$PROJECT_DIR"/"$contest_dir"/CMakeLists.txt
+PROJECT_NAME=$contest_dir envsubst \$PROJECT_NAME < "$CMAKE_TEMPLATE" > CMakeLists.txt
 for c in {A..Z} ; do
-  cat "$LIBRARY_DIR"/template.hpp > "$PROJECT_DIR"/"$contest_dir"/"$c".cpp
-  echo "add_executable(${contest_dir##*/}_${c} ${c}.cpp)" >> "$PROJECT_DIR"/"$contest_dir"/CMakeLists.txt
+  cat "$LIBRARY_DIR"/template.hpp > "$c".cpp
+  echo "add_executable(${contest_dir##*/}_${c} ${c}.cpp)" >> CMakeLists.txt
   (( ++i == file_num )) && break
 done
