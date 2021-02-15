e026bbef354a00ae5ccf34824016f8ced6bbea94
rajyan <kitakita7617@gmail.com>
2021-02-15 23:01:05 +0900

    ABC190-C

--- ABC/ABC190/C.cpp
+++ ABC/ABC190/C.cpp
@@ -11,6 +11,7 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <array>
 #include <vector>
 #include <set>
 #include <map>
@@ -34,8 +35,40 @@ struct init {
     }
 } init_;
 
+template<class T>
+inline bool chmax(T &a, const T b) { return a < b && (a = b, true); }
+
 int main() {
 
+    int N, M;
+    cin >> N >> M;
+    vector<array<int, 2>> AB(M);
+    for (int i = 0; i < M; i++) cin >> AB[i][0] >> AB[i][1];
+    for (int i = 0; i < M; i++) AB[i][0]--, AB[i][1]--;
+
+    int K;
+    cin >> K;
+    vector<array<int, 2>> CD(K);
+    for (int i = 0; i < K; i++) cin >> CD[i][0] >> CD[i][1];
+    for (int i = 0; i < K; i++) CD[i][0]--, CD[i][1]--;
+
+    int ans = 0;
+    for (int bit = 0; bit < (1 << K); bit++) {
+        vector<int> used(N);
+        for (int i = 0; i < K; i++) {
+            auto[c, d] = CD[i];
+            if ((bit >> i) & 1) used[c] = 1;
+            else used[d] = 1;
+        }
+        int now = 0;
+        for (int i = 0; i < M; i++) {
+            auto[a, b] = AB[i];
+            if (used[a] && used[b]) now++;
+        }
+        chmax(ans, now);
+    }
+    
+    cout << ans << '\n';
 
     return 0;
 }
