d6e52f093eee6422294e1cb26305f214a8483ef3
rajyan <kitakita7617@gmail.com>
2021-02-22 11:46:46 +0900

    ABC192-F

--- ABC/ABC192/F.cpp
+++ ABC/ABC192/F.cpp
@@ -34,12 +34,45 @@ struct init {
     }
 } init_;
 
+template<class T>
+vector<T> make_vec(size_t s, T val) { return vector<T>(s, val); }
+template<class... Size>
+auto make_vec(size_t s, Size... tail) {
+    return vector<decltype(make_vec(tail...))>(s, make_vec(tail...));
+}
+
+template<class T>
+inline bool chmax(T &a, const T b) { return a < b && (a = b, true); }
+template<class T>
+inline bool chmin(T &a, const T b) { return a > b && (a = b, true); }
+
 int main() {
 
     lint N, X;
     cin >> N >> X;
     vector<int> A(N);
     for (int i = 0; i < N; i++) cin >> A[i];
+
+    lint ans = LINF;
+    for (int m = 1; m <= N; m++) {
+        // 前からi個みて、j個使ったとき、k (mod m) の中での最大値
+        auto dp = make_vec(2, N + 1, m, -LINF);
+        dp[0][0][0] = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j <= i; j++) {
+                for (int k = 0; k < m; k++) {
+                    chmax(dp[(i + 1) & 1][j][k], dp[i & 1][j][k]);
+                    chmax(dp[(i + 1) & 1][j + 1][(k + A[i]) % m], dp[i & 1][j][k] + A[i]);
+                }
+            }
+        }
+        lint M = dp[N & 1][m][X % m];
+        if (M < 0) continue;
+        assert((X - M) % m == 0);
+        chmin(ans, (X - M) / m);
+    }
+
+    cout << ans << '\n';
 
     return 0;
 }
