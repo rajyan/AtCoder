92bc58b74321e5895c0d933c0eba0388df9c0fe4
rajyan <kitakita7617@gmail.com>
2021-02-28 11:03:27 +0900

    fix CmakeLists

--- create_list.sh
+++ create_list.sh
@@ -6,7 +6,7 @@ CMAKE_TEMPLATE=${CMAKE_TEMPLATE:-'/home/rajyan/CLion/AtCoder/CMakeLists_template
 # create CMakeLists
 CONTEST_DIR=${1?'Set directory name!'}
 CMAKE_FILE="$PROJECT_DIR"/"$CONTEST_DIR"/CMakeLists.txt
-PROJECT_NAME=$CONTEST_DIR envsubst < "$CMAKE_TEMPLATE" > "$CMAKE_FILE"
+PROJECT_NAME=$CONTEST_DIR envsubst \$PROJECT_NAME < "$CMAKE_TEMPLATE" > "$CMAKE_FILE"
 
 for file in "$PROJECT_DIR"/"$CONTEST_DIR"/*.cpp ; do
   name=${file##*/}
