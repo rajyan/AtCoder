1589f56fda81e8819cce15d36c46fc3d7e137b0a
rajyan <kitakita7617@gmail.com>
2021-03-19 09:15:50 +0900

    ABC195-E

--- ABC/ABC195/E.cpp
+++ ABC/ABC195/E.cpp
@@ -34,6 +34,81 @@ struct init {
     }
 } init_;
 
+template<class T>
+vector<T> make_vec(size_t s, T val) { return vector<T>(s, val); }
+template<class... Size>
+auto make_vec(size_t s, Size... tail) {
+    return vector<decltype(make_vec(tail...))>(s, make_vec(tail...));
+}
+
+template<const int &Modulo>
+struct Mint {
+
+    lint val;
+    constexpr Mint(lint v = 0) noexcept: val(v % Modulo) { if (val < 0) val += Modulo; }
+
+    constexpr Mint &operator+=(const Mint &r) noexcept {
+        val += r.val;
+        if (val >= Modulo) val -= Modulo;
+        return *this;
+    }
+    constexpr Mint &operator-=(const Mint &r) noexcept {
+        val -= r.val;
+        if (val < 0) val += Modulo;
+        return *this;
+    }
+    constexpr Mint &operator*=(const Mint &r) noexcept {
+        val = val * r.val % Modulo;
+        return *this;
+    }
+    constexpr Mint &operator/=(const Mint &r) noexcept {
+        lint a{r.val}, b{Modulo}, u{1}, v{0};
+        while (b) {
+            lint t = a / b;
+            a -= t * b;
+            a ^= b, b ^= a, a ^= b;
+            u -= t * v;
+            u ^= v, v ^= u, u ^= v;
+        }
+        assert(a == 1);
+        val = val * u % Modulo;
+        if (val < 0) val += Modulo;
+        return *this;
+    }
+
+    constexpr Mint operator+(const Mint &r) const noexcept { return Mint(*this) += r; }
+    constexpr Mint operator-(const Mint &r) const noexcept { return Mint(*this) -= r; }
+    constexpr Mint operator*(const Mint &r) const noexcept { return Mint(*this) *= r; }
+    constexpr Mint operator/(const Mint &r) const noexcept { return Mint(*this) /= r; }
+
+    constexpr Mint operator-() const noexcept { return Mint(-val); }
+
+    constexpr bool operator==(const Mint &r) const noexcept { return val == r.val; }
+    constexpr bool operator!=(const Mint &r) const noexcept { return !((*this) == r); }
+    constexpr bool operator<(const Mint &r) const noexcept { return val < r.val; }
+
+    constexpr friend ostream &operator<<(ostream &os, const Mint<Modulo> &x) noexcept { return os << x.val; }
+    constexpr friend istream &operator>>(istream &is, Mint<Modulo> &x) noexcept {
+        lint tmp{};
+        is >> tmp;
+        x = Mint(tmp);
+        return is;
+    }
+
+    [[nodiscard]] constexpr Mint pow(lint n) const noexcept {
+        Mint res{1}, tmp{*this};
+        while (n > 0) {
+            if (n & 1) res *= tmp;
+            tmp *= tmp;
+            n >>= 1;
+        }
+        return res;
+    }
+};
+
+constexpr int MOD = 7;
+using mint = Mint<MOD>;
+
 int main() {
 
     int N;
@@ -41,22 +116,22 @@ int main() {
     string S, X;
     cin >> S >> X;
 
-    vector<int> A(7), T(7);
-    A[0] = 1;
-    T[0] = 1;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < A.size(); j++) {
-            A[(i * 10) % 7] |= A[i];
-            if (X[i] == 'A') A[(i * 10 + S[i] - '0') % 7] |= A[i];
+    auto memo = make_vec(N, 7, -1);
+    auto dfs = [&](auto &&f, const int &i, const mint &m) -> bool {
+
+        if (i == N) return m == 0;
+        if (memo[i][m.val] != -1) return memo[i][m.val];
+
+        if (X[i] == 'T') {
+            return (memo[i][m.val] = f(f, i + 1, m * 10 + (S[i] - '0')) || f(f, i + 1, m * 10));
         }
-    }
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < T.size(); j++) {
-            T[(i * 10) % 7] |= T[i];
-            if (X[i] == 'T') T[(i * 10 + S[i] - '0') % 7] |= T[i];
+        else {
+            return (memo[i][m.val] = f(f, i + 1, m * 10 + (S[i] - '0')) && f(f, i + 1, m * 10));
         }
-    }
-    DMP(A, T);
+    };
+
+    if (dfs(dfs, 0, 0)) cout << "Takahashi\n";
+    else cout << "Aoki\n";
 
     return 0;
 }
