b27d53b2047f7d68a266024f986c4ea13db1241d
rajyan <kitakita7617@gmail.com>
2021-03-31 08:48:36 +0900

    ABC194-E

--- ABC/ABC194/E.cpp
+++ ABC/ABC194/E.cpp
@@ -34,8 +34,39 @@ struct init {
     }
 } init_;
 
+template<class T>
+inline bool chmin(T &a, const T b) { return a > b && (a = b, true); }
+
 int main() {
 
+    int N, M;
+    cin >> N >> M;
+
+    vector<int> A(N);
+    for (int i = 0; i < N; i++) cin >> A[i];
+
+    vector<int> cnt(N + 1);
+    // fill cnt for [0, M)
+    for (int i = 0; i < M; i++) {
+        cnt[A[i]]++;
+    }
+    int ans = N + 1;
+    // find initial mex
+    for (int i = 0; i <= N; i++) {
+        if (cnt[i] == 0) {
+            ans = i;
+            break;
+        }
+    }
+
+    // slide window
+    for (int i = 0; i + M < N; i++) {
+        cnt[A[i + M]]++;
+        cnt[A[i]]--;
+        if (cnt[A[i]] == 0) chmin(ans, A[i]);
+    }
+
+    cout << ans << '\n';
 
     return 0;
 }
